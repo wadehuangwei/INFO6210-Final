c1607b8c897a8fbcae4ae87b5cfc706bb943a6e4
weili6 <li.wei6@husky.neu.edu>
2016-12-05 01:43:35 -0500

    add distance matrix function

--- register.php
+++ register.php
@@ -6,7 +6,6 @@ $db = mysqli_connect("localhost", "root", "", "healthcare");
 
 if (isset($_POST['register_btn']))
 {
-	session_start();
 
 	$username = mysql_real_escape_string($_POST['username']);
 	$email = mysql_real_escape_string($_POST['email']);
@@ -16,7 +15,6 @@ if (isset($_POST['register_btn']))
     $street = mysql_real_escape_string($_POST['street']);
     $city = mysql_real_escape_string($_POST['city']);
     $state = mysql_real_escape_string($_POST['state']);
-    $country = mysql_real_escape_string($_POST['country']);
     $zipcode = mysql_real_escape_string($_POST['zipcode']);
 
     $accountType = mysql_real_escape_string($_POST['accountType']);
@@ -51,6 +49,53 @@ if($accountType == "Doctor"){
 } 
 
 if($accountType =="Patient"){
+
+$sql = "SELECT UserAccount.UserID, Address.Street, Address.City, Address.State FROM Address INNER JOIN UserAccount ON Address.AddressID = UserAccount.AddressID WHERE UserAccount.Username = '$username'";
+$result = mysqli_query($db, $sql);
+
+if(mysqli_num_rows($result) == 1){
+        $row = mysqli_fetch_assoc($result);
+        $patientID = $row['UserID'];
+        $street = $row['Street'];
+        $city = $row['City'];
+        $state = $row['State'];
+        $min = 1000000000000;
+        $maxID = '';
+
+        $sql_wh = "SELECT Warehouse.WarehouseID, Address.Street, Address.City, Address.State FROM Warehouse INNER JOIN Address ON Address.AddressID = Warehouse.AddressID";
+        $result_wh = mysqli_query($db, $sql_wh);
+        while($row_wh = mysqli_fetch_assoc($result_wh)){
+            $street_wh = $row_wh['Street'];
+            $city_wh = $row_wh['City'];
+            $state_wh = $row_wh['State'];
+            $warehouseID = $row_wh['WarehouseID'];
+
+            $coordinates1 = get_coordinates($city, $street, $state);
+            $coordinates2 = get_coordinates($city_wh, $street_wh, $state_wh);
+            if ( !$coordinates1 || !$coordinates2 ){
+               echo 'Bad address.';
+            }
+            else{
+               $dist = GetDrivingDistance($coordinates1['lat'], $coordinates2['lat'], $coordinates1['long'], $coordinates2['long']);
+               if(floatval($min) > floatval($dist['distance'])){
+                $min = $dist['distance'];
+                $minID = $warehouseID;
+                }
+            }
+            
+        }
+        if(!empty($minID)){
+            $minID = mysql_real_escape_string($minID);
+            $sql_update = "INSERT INTO Patient(PatientID, NearestWarehouseID) VALUES ('$patientID', '$minID')";
+            mysqli_query($db, $sql_update);
+        }
+        
+    } else
+    {
+        $_SESSION['message'] = "User address doesn't exist.";
+    }
+
+
 header("location: homePage.php");
 }
 
@@ -63,6 +108,50 @@ else
 
 }
 	}
+
+function get_coordinates($city, $street, $province)
+{
+    $address = urlencode($city.','.$street.','.$province);
+    $url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=us";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $response_a = json_decode($response);
+    $status = $response_a->status;
+
+    if ( $status == 'ZERO_RESULTS' )
+    {
+        return FALSE;
+    }
+    else
+    {
+        $return = array('lat' => $response_a->results[0]->geometry->location->lat, 'long' => $long = $response_a->results[0]->geometry->location->lng);
+        return $return;
+    }
+}
+
+function GetDrivingDistance($lat1, $lat2, $long1, $long2)
+{
+    $url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$lat1.",".$long1."&destinations=".$lat2.",".$long2."&mode=driving&language=en";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $response_a = json_decode($response, true);
+    $dist = $response_a['rows'][0]['elements'][0]['distance']['text'];
+    $time = $response_a['rows'][0]['elements'][0]['duration']['text'];
+
+    return array('distance' => $dist, 'time' => $time);
+}
 ?>
 
 <!DOCTYPE html>
